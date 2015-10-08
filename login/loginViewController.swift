474a07daebc7126e2b19cacb121a1a4898bbfc2f
zhq <zhq@iminido.com>
2015-10-09 01:25:00 +0800

    init

---
+++ login/loginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  iMe
+//
+//  Created by Qyee on 15/10/8.
+//  Copyright © 2015年 珠海赞友科技有限公司. All rights reserved.
+//
+
+import Foundation
+
+class loginViewController : NSObject {
+
+}
